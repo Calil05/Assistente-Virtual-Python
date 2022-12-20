8f7c2a5dd3e908264cadaf042b218f3283824321
calil05 <gabrielbcalil@gmail.com>
2022-12-20 17:29:46 -0300

    Version 0.3

--- lists.py
+++ lists.py
@@ -1,7 +1,14 @@
-time_list = ["Que horas são","Que horas são agora","horas","Me diga as horas","Me diga que horas são"]
+time_list = ["que horas são","que horas são agora","horas","me diga as horas","me diga que horas são"]
 
-date_list = ["Que dia é hoje","Qual a data de hoje","data","Qual é a data atual","Qual é a data de hoje","Que data é hoje","em que data é hoje","em que dia é hoje","em qual é a data de hoje"]
+date_list = ["que dia é hoje","qual a data de hoje","data","qual é a data atual","qual é a data de hoje","que data é hoje","em que data é hoje","em que dia é hoje","em qual é a data de hoje"]
 
-notepad_list = ["Abrir bloco de notas","Bloco de notas","Abra o bloco de notas"]
+notepad_list = ["abrir bloco de notas","bloco de notas","abra o bloco de notas"]
 
-chorme_list = ["Abrir google chrome","Abrir google","Abrir chrome","Abrir Chrome","chrome","google"]
+chorme_list = ["abrir google chrome","abrir google","abrir chrome","abrir Chrome","chrome","google"]
+
+joke_list = ["conte uma piada","fale uma piada","piada","me conte uma piada","me fale uma piada","me conte outra piada","conte outra piada"]
+
+jokes = ["Se o pato perde a pata, ele fica manco ou viuvo? Ele fica triste","Piu","Porque o menino estava falando ao telefone deitado? Pra não cair a ligação","Qual é a fórmula da água benta? H Deus O!",
+"Relam estava devendo para Marquinhos. Oque aconteceu depois? Relam pagou Marquinhos","Qual é a cidade que não tem táxi? Uberlandia","Porque o jacaré tirou o filho da escola? Porque ele réptil de ano","Por que o policial não usa sabão? Porque ele prefere deter gente",
+"Qual estado do Brasil é um trem? O PIAUIIIIIII","Como se faz omelete de chocolate? Com ovos de Pascoa","O que é um pontinho preto no avião? Uma aeromosca","Um tênis foi jogado ao mar e afundou. Qual o nome do filme?, Titanike",
+"Qual é o tio da construção? O Tio Jolo","O que um álcool disse para outro álcool? Etanóis"]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import speech_recognition as sr
 import pyttsx3
 import os
+from random import choice
 from date_time import get_time, get_date
 from lists import*
 
@@ -21,18 +22,22 @@ while True:
         audio = reco.listen(source)
 
         speech = reco.recognize_google(audio, language='pt')
-        if speech in time_list:
+        if speech.lower() in time_list:
             time = get_time()
             speak(time)
-        elif speech in date_list:
+        elif speech.lower() in date_list:
             date = get_date()
             speak(date)
-        elif speech in notepad_list:
+        elif speech.lower() in notepad_list:
             speak("Abrindo Notepad")
             os.system('notepad.exe')
-        elif speech in chorme_list:
+        elif speech.lower() in chorme_list:
             speak("Abrindo Google Chrome")
             os.system('"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"')
+        elif speech.lower() in joke_list:
+            rand = choice(jokes)
+            speak("Aqui vai uma piada")
+            speak(rand)
         else:
             print(speech)
             speak("Não entendi oque você disse")
